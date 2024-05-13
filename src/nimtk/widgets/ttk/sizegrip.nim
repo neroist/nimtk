cef53650125cb762851033719aeb476f461bde73
neroist <pmpiga1@gmail.com>
2024-05-13 19:46:56 -0400

    implement ttk::sizegrip

---
+++ src/nimtk/widgets/ttk/sizegrip.nim
@@ -0,0 +1,22 @@
+import std/strutils
+
+import ../utils/escaping
+import ../utils/genname
+import ../../nimtk
+import ./widget
+
+type
+  Sizegrip* = ref object of TtkWidget
+
+proc isSizegrip*(w: Widget): bool = "sizegrip" in w.pathname.split('.')[^1]
+
+proc newSizegrip*(parent: Widget, configuration: openArray[(string, string)] = {:}): Sizegrip =
+  new result
+
+  result.pathname = pathname(parent.pathname, genName("sizegrip_"))
+  result.tk = parent.tk
+
+  result.tk.call("ttk::sizegrip", result.pathname)
+
+  if configuration.len > 0:
+    result.configure(configuration)
