1cc940898d5a77606d18f09ee814afd29c2b4ecd
neroist <pmpiga1@gmail.com>
2024-05-13 20:40:29 -0400

    ttk:separator

---
+++ src/nimtk/widgets/ttk/separator.nim
@@ -0,0 +1,23 @@
+import std/strutils
+
+import ../../utils/escaping
+import ../../utils/genname
+import ../../../nimtk
+import ../widget
+import ./widget
+
+type
+  Separator* = ref object of TtkWidget
+
+proc isSeparator*(w: Widget): bool = "separator" in w.pathname.split('.')[^1]
+
+proc newSeparator*(parent: Widget, configuration: openArray[(string, string)] = {:}): Separator =
+  new result
+
+  result.pathname = pathname(parent.pathname, genName("separator_"))
+  result.tk = parent.tk
+
+  result.tk.call("ttk::separator", result.pathname)
+  
+  if configuration.len > 0:
+    result.configure(configuration)
